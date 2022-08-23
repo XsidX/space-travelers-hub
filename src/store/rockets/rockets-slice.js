497e458a8bffbd19e4249c4444d695487d82bf0c
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-08-23 11:31:52 +0200

    Add rocket slice reducer

---
+++ src/store/rockets/rockets-slice.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+import { getRockets } from '../actions';
+
+const initialState = {
+  rockets: [],
+  status: null,
+};
+
+const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getRockets.pending]: (state) => {
+      state.status = 'loading rockets';
+    },
+    [getRockets.fulfilled]: (state, action) => {
+      state.missions = action.payload;
+      state.status = 'loading rockets complete';
+    },
+    [getRockets.rejected]: (state) => {
+      state.status = 'fetching rockets failed';
+    },
+  },
+});
+
+export const rocketsActions = rocketsSlice.actions;
+
+export default rocketsSlice.reducer;
