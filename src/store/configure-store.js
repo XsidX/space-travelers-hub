769594c2aad16aa0ac4e3488e5ad40651ea92e74
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-08-23 11:34:48 +0200

    add recket reducer to th store

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from './missions/missions-slice';
+import rocketsReducer from './rockets/rockets-slice';
 
 const store = configureStore({
   reducer: {
     missions: missionsReducer,
+    rockets: rocketsReducer,
   },
 });
 
